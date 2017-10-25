422f683864eff791b741b0e9611b5f14cabb632c
lromang <luis.roangarci@gmail.com>
2017-10-25 09:39:04 -0500

    users

--- Workspace/tango_with_django_project/rango/urls.py
+++ Workspace/tango_with_django_project/rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 
 ## Url Patterns
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$',     views.index, name = 'index'),
+    url(r'^users', views.users, name = 'users'),
     url(r'^about', views.about, name = 'about'),
 ]
--- Workspace/tango_with_django_project/rango/views.py
+++ Workspace/tango_with_django_project/rango/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http      import HttpResponse
 
-## First View
+## Index View
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context = context_dict)
 
-## Second View
+## About View
 def about(request):
     return HttpResponse('This is the about page')
+
+## Users
+def users(request):
+    context_dict = {'username': 'Luis'}
+    return render(request, 'rango/users.html', context = context_dict)
